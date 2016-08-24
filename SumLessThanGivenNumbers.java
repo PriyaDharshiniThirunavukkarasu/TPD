6d53f248db264bf18a24866fd751739bae2518c4
PriyaDharshiniThirunavukkarasu <priya.dharshinicse@ksrce.ac.in>
2016-08-24 16:52:44 +0530

    Create SumLessThanGivenNumbers.java

---
+++ SumLessThanGivenNumbers.java
@@ -0,0 +1,11 @@
+public class SumLessThanGivenNumbers {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		int answer = 0;
+		for (int i = 1; i <= number; i++) {
+			answer += i;
+		}
+		System.out.println(answer);
+	}
+}
