bdf1c133ece7d752ae98558d0e38dfb2ea8e5b9f
PriyaDharshiniThirunavukkarasu <priya.dharshinicse@ksrce.ac.in>
2016-09-01 10:24:45 +0530

    Create Pro15.java

---
+++ Pro15.java
@@ -0,0 +1,42 @@
+
+public class Pro15 {
+	public static void main(String[] args) {
+		int[] input = { 31, 14, 15, 7, 2, 4, 3 };
+		String[] binary = new String[input.length];
+		for (int i = 0; i < input.length; i++) {
+			binary[i] = Integer.toBinaryString(input[i]);
+		}
+		int[] count = new int[input.length];
+		for (int i = 0; i < input.length; i++) {
+			for (int j = 0; j < binary[i].length(); j++) {
+				if (binary[i].charAt(j) == '1') {
+					count[i]++;
+				}
+			}
+		}
+		for (int i = 0; i < count.length; i++) {
+			for (int j = i + 1; j < count.length; j++) {
+				if (count[i] < count[j]) {
+					int temp = count[i];
+					count[i] = count[j];
+					count[j] = temp;
+					temp = input[i];
+					input[i] = input[j];
+					input[j] = temp;
+				}
+			}
+		}
+		for (int i = 0; i < count.length; i++) {
+			for (int j = i + 1; j < count.length; j++) {
+				if (count[i] == count[j] && input[i] < input[j]) {
+					int temp = input[i];
+					input[i] = input[j];
+					input[j] = temp;
+				}
+			}
+		}
+		for (int i = 0; i < input.length; i++) {
+			System.out.print(input[i] + "  ");
+		}
+	}
+}
